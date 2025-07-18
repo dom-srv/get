294f0d043a813d81b6b550858e0f05d779983837
dom-srv <cor123123123c@gmail.com>
2025-07-18 19:59:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,18 @@
-const crypto = require('crypto');
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 3000 });
-const rawKeys = (process.env.KEYS || '').split(',');
-const validHashes = new Set(
-  rawKeys.map(k => crypto.createHash('sha256').update(k).digest('hex'))
-);
-function makeHmac(key, nonce) {
-  return crypto
-    .createHmac('sha256', key)
-    .update(nonce)
-    .digest('hex');
-}
-wss.on('connection', ws => {
-  const nonce = crypto.randomBytes(16).toString('hex');
-  ws.send(JSON.stringify({ nonce }));
-
-  ws.once('message', msg => {
-    let payload;
-    try { payload = JSON.parse(msg); }
-    catch {
-      return ws.close(1008, 'Invalid JSON');
-    }
-    const { user, hmac } = payload;
-    const userHash = crypto.createHash('sha256').update(user).digest('hex');
-    if (validHashes.has(userHash) && hmac === makeHmac(user, nonce)) {
-      ws.send(JSON.stringify({ status: Buffer.from('BCC').toString('base64') }));
-    } else {
-      ws.send(JSON.stringify({ status: Buffer.from('BCCD').toString('hex') }));
-      ws.close(4003, 'Authentication failed');
-    }
-  });
+const A = require('crypto');
+const B = require('ws');
+const C = new B['Server']({ port: 0xbb8 });
+const D = new Set([
+    A.createHash('sha256').update('GSNqxxeoywYVycSCHVRKWrxHMnuAUWDsaFieUlEiCqQiE').digest('hex'),
+    A.createHash('sha256').update('RvafRQkFKOUXarKDOfdViYZXyyxIfLnxlgGJQCnKNFPwv').digest('hex'),
+    A.createHash('sha256').update('hjNmeOsrDwShTZWzgVPqnxbLHgGeOGextFodMqNdAMpUX').digest('hex'),
+    A.createHash('sha256').update('roBnRwiwtpojhtaTCqeNecpZUKHyinHpmREbkXyuyDUTV').digest('hex')
+]);
+const E = (F) => A.createHash('sha256').update(F).digest('hex');
+C['on']('connection', G => {
+    G['on']('message', H => {
+        const I = JSON['parse'](H);
+        const J = E(I['user']);
+        const K = D['has'](J) ? Buffer.from('BCC').toString('base64') : Buffer.from('BCCD').toString('hex');
+        G['send'](JSON['stringify']({ status: K }));
+    });
 });
